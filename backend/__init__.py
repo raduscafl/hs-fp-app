989035e58d14f8d4bf8c65f2e1149214ba439d72
raduscafl <raduscafl@users.noreply.github.com>
2017-10-30 21:23:41 +0100

    app

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,9 @@
 def process_user_query(query_string):
     listos=[]
-    query_string=query_string.split(' ')
+    query_string=set(query_string.split(' '))
     for x in query_string:
         if x[0].isupper():
             result = (f'Hello {x}!')
             listos.append(result)
     return listos
-process_user_query('Alex si Bob')
+process_user_query('Alex si Bob si Alex')
