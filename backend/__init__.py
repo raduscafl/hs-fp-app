9831a7aa21c3e452b2f48d517ac23821962331cb
raduscafl <raduscafl@users.noreply.github.com>
2017-11-01 14:35:45 +0100

    app

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,27 +1,4 @@
-languages=["Acèh", "Адыгабзэ", "Afrikaans", "Akan", "Alemannisch", "አማርኛ", "Ænglisc", "Аҧсшәа", "العربية", "Aragonés", "ܐܪܡܝܐ", "Armãneashti", "Arpetan", "অসমীয়া", "Asturianu", "Atikamekw", "Avañe'ẽ", "Авар", "Aymar aru", "Azərbaycanca", "تۆرکجه", "Bamanankan", "বাংলা", "Bahasa Banjar", "Bân-lâm-gú", "Basa Banyumasan", "Башҡортса", "Беларуская", "Беларуская (тарашкевіца)‎", "भोजपुरी", "Bikol Central", "Bislama", "Български", "Boarisch", "བོད་ཡིག", "Bosanski", "Brezhoneg", "Буряад", "Català", "Чӑвашла", "Cebuano", "Čeština", "Chamoru", "Chavacano de Zamboanga", "ChiShona", "Corsu", "Cymraeg", "Dansk", "Davvisámegiella", "Deitsch", "Deutsch", "ދިވެހިބަސް", "Dolnoserbski", "Eesti", "Ελληνικά", "Emiliàn e rumagnòl", "Эрзянь", "Español", "Esperanto", "Estremeñu", "Euskara", "فارسی", "Føroyskt", "Français", "Frysk", "Fulfulde", "Furlan", "Gaeilge", "Gaelg", "Gagauz", "Gàidhlig", "Galego", "贛語", "Gĩkũyũ", "گیلکی", "ગુજરાતી", "𐌲𐌿𐍄𐌹𐍃𐌺", "गोंयची कोंकणी / Gõychi Konknni", "客家語/Hak-kâ-ngî", "Хальмг", "한국어", "Hausa", "Hawaiʻi", "Հայերեն", "हिन्दी", "Hornjoserbsce", "Hrvatski", "Ido", "Igbo", "Ilokano", "বিষ্ণুপ্রিয়া মণিপুরী", "Bahasa Indonesia", "Interlingua", "Interlingue", "ᐃᓄᒃᑎᑐᑦ/inuktitut", "Iñupiak", "Ирон", "IsiXhosa", "IsiZulu", "Íslenska", "Italiano", "עברית", "Basa Jawa", "Kalaallisut", "ಕನ್ನಡ", "Kapampangan", "Къарачай-малкъар", "ქართული", "कॉशुर / کٲشُر", "Kaszëbsczi", "Қазақша", "Kernowek", "Kiswahili", "Kreyòl ayisyen", "Kurdî", "Кыргызча", "Кырык мары", "Ladino", "Лакку", "Лезги", "ລາວ", "لۊری شومالی", "Latgaļu", "Latina", "Latviešu", "Lëtzebuergesch", "Lietuvių", "Ligure", "Limburgs", "Lingála", "Livvinkarjala", "La .lojban.", "Luganda", "Lumbaart", "Magyar", "मैथिली", "Македонски", "Malagasy", "മലയാളം", "Malti", "Māori", "मराठी", "მარგალური", "مصرى", "مازِرونی", "Bahasa Melayu", "Baso Minangkabau", "Mìng-dĕ̤ng-ngṳ̄", "Mirandés", "Мокшень", "Молдовеняскэ", "Монгол", "မြန်မာဘာသာ", "Nāhuatl", "Dorerin Naoero", "Nederlands", "Nedersaksies", "नेपाली", "नेपाल भाषा", "日本語", "Napulitano", "Нохчийн", "Nordfriisk", "Norfuk / Pitkern", "Norsk", "Norsk nynorsk", "Nouormand", "Novial", "Occitan", "Олык марий", "ଓଡ଼ିଆ", "Oshiwambo", "Oʻzbekcha/ўзбекча", "ਪੰਜਾਬੀ", "पालि", "Pälzisch", "Pangasinan", "پنجابی", "Papiamentu", "پښتو", "Patois", "Перем Коми", "ភាសាខ្មែរ", "Picard", "Piemontèis", "Tok Pisin", "Plattdüütsch", "Polski", "Ποντιακά", "Português", "Qafár af", "Qaraqalpaqsha", "Qırımtatarca", "Reo tahiti", "Ripoarisch", "Română", "Romani", "Rumantsch", "Runa Simi", "Русиньскый", "Русский", "Саха тыла", "Gagana Samoa", "संस्कृतम्", "Sängö", "Sardu", "Scots", "Seeltersk", "Sesotho sa Leboa", "Setswana", "Shqip", "Sicilianu", "සිංහල", "Simple English", "سنڌي", "SiSwati", "Slovenčina", "Slovenščina", "Словѣньскъ / ⰔⰎⰑⰂⰡⰐⰠⰔⰍⰟ", "Ślůnski", "Soomaaliga", "کوردی", "Sranantongo", "Српски / srpski", "Srpskohrvatski / српскохрватски", "Basa Sunda", "Suomi", "Svenska", "Tagalog", "தமிழ்", "Taqbaylit", "Tarandíne", "Татарча/tatarça", "తెలుగు", "Tetun", "ไทย", "ትግርኛ", "Тоҷикӣ", "ᏣᎳᎩ", "Tsetsêhestâhese", "Tshivenda", "Türkçe", "Türkmençe", "Twi", "Тыва дыл", "Удмурт", "ᨅᨔ ᨕᨘᨁᨗ", "Українська", "اردو", "ئۇيغۇرچە / Uyghurche", "Vahcuengh", "Vèneto", "Vepsän kel’", "Tiếng Việt", "Volapük", "Võro", "Walon", "文言", "West-Vlams", "Winaray", "Wolof", "吴语", "Xitsonga", "ייִדיש", "Yorùbá", "粵語", "Zazaki", "Zeêuws", "Žemaitėška", "中文", "डोटेली", "Kabɩyɛ"]
-
 def process_user_query(query_string):
-    language=[]
-    user_want=query_string.split(' ')
-    user_want=user_want[0].lower()
-
-    if user_want=="language":
-        query_string=query_string.split(' ')
-        i=query_string.index("Languages")
-        i=i+1
-
-        for lang in languages:
-            if query_string[i]=="Edit":
-                break
-            else:
-                if query_string[i]==lang:
-                    language.append(query_string[i])
-                    i=i+1
-                else:
-                    break
-        return language
-
-    elif user_want=="populations":
         population=[]
         query_string=query_string.split(' ')
         city=query_string.index("(disambiguation).")
@@ -29,19 +6,8 @@ def process_user_query(query_string):
         i=query_string.index("Population")
         ix=query_string.index("Demonym(s)")
         i=i+2
+        population.append("Population:")
         for pop in query_string[i:ix]:
             population.append(pop)
         return population
-
-    else:
-        pass
-
-#process_user_query('Languages mama pipi Languages Afrikaans Alemannisch Edit links This page was last edited on 30 October 2017, at 06:31. Text is available under the Creative Commons Attribution-ShareAlike License; additional terms may apply. By using this site, you agree to the Terms of Use and Privacy Policy. Wikipedia® is a registered trademark of the Wikimedia Foundation, Inc., a non-profit organization. Privacy policyAbout WikipediaDisclaimersContact WikipediaDevelopersCookie statementMobile viewEnable previews Wikimedia Foundation Powered by MediaWiki')
-
-#process_user_query("Populations (disambiguation). Bucale Elevation 667 m (2,188 ft) Population (2015[4])• Municipality 3,141,991 Demonym(s) Londoner Cockney (colloquial)")
-
-#    elif :user_want=="contents"
-
-#    elif :user_want=="notes"
-
-#    elif :user_want=="references"
+process_user_query("Populations (disambiguation). Bucale Elevation 667 m (2,188 ft) Population (2015[4])• Municipality 3,141,991 Demonym(s) Londoner Cockney (colloquial)")
